cb75d1478d0991eb5d023163e53109b1db3875fa
Diyonsebastian <155514532+Diyonsebastian@users.noreply.github.com>
2024-01-03 12:35:37 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+const monkey = require("monkey");
 
 const connectDatabase = () => {
-  mongoose
+monkey
     .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
